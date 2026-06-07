851b627bae22bb0ab3beb3f970af144177534248
Xsenia <kseniakifak@icloud.com>
2026-06-07 12:30:16 +0300

    gjgvgch

---
+++ flores/src/test/java/com/impact/flores/FloresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
